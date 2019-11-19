f9d11e3ba23439508d2680551e6a9b5ea898188c
RuStyC0der <miller_maers@tutanota.com>
2019-11-19 15:24:23 +0200

    added swich

--- src/js/main.js
+++ src/js/main.js
@@ -28,3 +28,14 @@ document.getElementsByClassName("login__button")[0].addEventListener('click', fu
     console.log("name: " + document.getElementsByClassName('login__name')[0].value)
     console.log("pass: " + document.getElementsByClassName('login__pass')[0].value)
 });
+
+document.getElementsByClassName('ass')[0].addEventListener('click', function () {
+     if (this.checked){
+        
+        document.getElementsByClassName('form__registration')[0].style.display = "none";
+        document.getElementsByClassName('form__login')[0].style.display = "flex";
+     }else{
+        document.getElementsByClassName('form__registration')[0].style.display = "flex";
+        document.getElementsByClassName('form__login')[0].style.display = "none";
+     }
+});
